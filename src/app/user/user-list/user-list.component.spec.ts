b9dfbd1e658aff1663ff0f32efbaeb8c74e8b9c7
Gihan Wijesinghe <gihantharaka76@gmail.com>
2025-05-08 07:38:54 +0530

    unit tests added for the user-list comp

--- src/app/user/user-list/user-list.component.spec.ts
+++ src/app/user/user-list/user-list.component.spec.ts
@@ -1,21 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserListComponent } from './user-list.component';
+import { UserService } from '../user.service';
+import { of } from 'rxjs';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
-  let fixture: ComponentFixture<UserListComponent>;
+  let fixture: ComponentFixture<UserListComponent>; // wrapper around the component to test
+  let userService: UserService;
+  let userServiceSpy: jasmine.Spy;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [UserListComponent]
-    });
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      declarations: [UserListComponent],
+      providers: [UserService]
+    }).compileComponents(); // compile the component and its template
+
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    userService = TestBed.inject(UserService);
+    userServiceSpy = spyOn(userService, 'getUsers').and.returnValue(of([
+      { id: 1, name: "John Doe"},
+      { id: 2, name: "Maria Doe"},
+    ]))
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should retrieve users from UserService on init', () => {
+    fixture.detectChanges(); // trigger ngOnInit
+    expect(userServiceSpy).toHaveBeenCalled();
+  });
+
 });
